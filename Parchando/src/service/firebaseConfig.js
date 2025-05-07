0e7e3193338a42b942579d13ab8f663bce19df00
Michael Rodriguez Rios <michael_rios05@hotmail.com>
2025-05-07 16:05:57 -0500

    Actualizacion en firebase y dependencias
    cambio temporal en base de datos para verificar datos de forma local

--- Parchando/src/service/firebaseConfig.js
+++ Parchando/src/service/firebaseConfig.js
@@ -1,25 +1,30 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB3RSwCBIiqstPaYWdqmwT3JzeFkVOdyGk",
-    authDomain: "parchando-abb40.firebaseapp.com",
-    projectId: "parchando-abb40",
-    storageBucket: "parchando-abb40.firebasestorage.app",
-    messagingSenderId: "1016879957228",
-    appId: "1:1016879957228:web:0d49ae205a855ae62a11c9",
-    measurementId: "G-WW523TM3BR"
+  apiKey: "AIzaSyANxGrUQfB8s-J8LTrNvq0XHsWmp3M9ynA",
+  authDomain: "realtime2025-998d8.firebaseapp.com",
+  databaseURL: "https://realtime2025-998d8-default-rtdb.firebaseio.com",
+  projectId: "realtime2025-998d8",
+  storageBucket: "realtime2025-998d8.firebasestorage.app",
+  //storageBucket: "realtime2025-998d8.appspot.com",
+  messagingSenderId: "752746460964",
+  appId: "1:752746460964:web:d3b16a63131436224d65b1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-// Obtiene y exporta la instancia de auth
+
+// Inicializa Auth con AsyncStorage para que persista el login
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
+  persistence: getReactNativePersistence(AsyncStorage)
 });
 
-export { app, db, auth };
+// Para subir/descargar imágenes
+const storage = getStorage(app);
+
+export { app, db, auth, storage };
